62f4fa763a05abb728e0ff8743f83f7f5bf25f60
lijainhao-ljh <122572191@qq,com>
2020-02-02 20:45:46 +0800

    提交

--- dzwble/src/main/java/com/accp/service/test.java
+++ dzwble/src/main/java/com/accp/service/test.java
@@ -1,7 +1,39 @@
 package com.accp.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.accp.domain.Carbrand;
+import com.accp.domain.CarbrandExample;
+import com.accp.mapper.CarbrandMapper;
+import com.accp.domain.CarbrandExample.Criteria;
+
+@Service
+@Transactional
 public class test {
-	public static void main(String[] args) {
-		System.out.println("小彭来了");
+	@Autowired
+	CarbrandMapper carbrandMapper;
+	/*
+	 * 模糊查询
+	 */
+	public List<Carbrand> findLikeCarbrand(String condition){
+		CarbrandExample example =new CarbrandExample();
+		Criteria criteria=example.createCriteria();
+		if (condition != null && !"".equals(condition.trim()))
+		{
+			//不为空
+			if(condition.matches("^[0-9]*$")) {
+				//数字根据编号查
+				example.createCriteria().andCbnoEqualTo(Integer.valueOf(condition));
+			}else {
+				//非数字根据编号查或是名称或首字母
+				example.or().andCbfirstLike("%"+condition+"%");
+				example.or().andCbnameLike("%"+condition+"%");
+			}
+		}
+		return carbrandMapper.selectByExample(example);
 	}
 }
