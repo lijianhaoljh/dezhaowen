f03a8da316bad455d2bd52564f03f32c14c6c4c1
pengyumei <iyumeipeng@163.com>
2020-02-02 19:09:14 +0800

    小彭测试

--- dzwble/src/main/java/com/accp/controller/testController.java
+++ dzwble/src/main/java/com/accp/controller/testController.java
@@ -1,5 +1,30 @@
 package com.accp.controller;
 
-public class testController {
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accp.domain.People;
+
+@RestController
+public class testController {
+	
+	@PostMapping("/loginin")
+	public Map<String, Object> loginin(String username, String password, HttpSession session) {
+		People users = new People();
+		users.setPname(username);
+		users.setPno(password);
+		
+		session.setAttribute("user", users);
+		Map<String, Object> map = new HashMap<>();
+		map.put("user", users);
+		map.put("token", session.getId());
+		System.out.println("进来了吧");
+		return map;
+	}
+	
 }
