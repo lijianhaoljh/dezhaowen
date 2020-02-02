6ac8db3643a92100413aab9aec97a29f2eb87871
lijainhao-ljh <122572191@qq,com>
2020-02-02 16:47:35 +0800

    1111

--- dzwble/src/main/java/com/accp/service/test.java
+++ dzwble/src/main/java/com/accp/service/test.java
@@ -1,7 +1,22 @@
 package com.accp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.accp.domain.CarbrandExample;
+import com.accp.domain.StaffExample;
+import com.accp.mapper.StaffMapper;
+import com.accp.domain.CarbrandExample.Criteria;
+
+@Service
 public class test {
-	public static void main(String[] args) {
-		System.out.println("小彭来了");
+	
+	@Autowired
+	StaffMapper staffMapper;
+	
+	public void me() {
+		CarbrandExample example =new CarbrandExample();
+		Criteria criteria=example.createCriteria();
+		
 	}
 }
